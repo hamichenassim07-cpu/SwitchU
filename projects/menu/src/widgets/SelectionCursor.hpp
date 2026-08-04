94084fcf5a23ba362af7e480b258a84caee431db
hamichenassim07-cpu <hamiche.nassim07@gmail.com>
2026-08-04 07:29:46 +0200

    Update SelectionCursor.hpp

--- projects/menu/src/widgets/SelectionCursor.hpp
+++ projects/menu/src/widgets/SelectionCursor.hpp
@@ -2,7 +2,6 @@
 #include <nxui/widgets/Widget.hpp>
 #include <nxui/core/Animation.hpp>
 
-
 class SelectionCursor : public nxui::Widget {
 public:
     SelectionCursor();
@@ -10,23 +9,31 @@ public:
     void moveTo(const nxui::Rect& target, float duration = 0.2f);
     void moveTo(const nxui::Rect& target, float cornerRadius, float duration);
     nxui::Rect currentRect() const;
+
     void setColor(const nxui::Color& c) { m_color = c; }
-    void setCornerRadius(float r)  { m_cornerRadius.setImmediate(r); }
-    void setBorderWidth(float w)   { m_borderWidth = w; }
+    void setCornerRadius(float r) { m_cornerRadius.setImmediate(r); }
+    void setBorderWidth(float w) { m_borderWidth = w; }
+
+    // Applications use the animated purple/fuchsia/blue gradient.
+    // Profiles and system buttons keep a quieter single-colour cursor.
+    void setGradientEnabled(bool enabled) { m_gradientEnabled = enabled; }
+    bool gradientEnabled() const { return m_gradientEnabled; }
 
 protected:
     void onUpdate(float dt) override;
     void onRender(nxui::Renderer& ren) override;
 
 private:
-    float computeAdaptiveDuration(const nxui::Rect& target, float targetCornerRadius, float baseDuration) const;
+    float computeAdaptiveDuration(const nxui::Rect& target,
+                                  float targetCornerRadius,
+                                  float baseDuration) const;
 
     nxui::AnimatedFloat m_x, m_y, m_w, m_h;
     nxui::AnimatedFloat m_cornerRadius;
     nxui::Color m_color {0.f, 0.75f, 1.f, 1.f};
-    float m_borderWidth  = 3.f;
+    float m_borderWidth = 3.f;
     float m_time = 0.f;
     float m_waveSpeed = 3.5f;
-    bool  m_initialized = false;
+    bool m_initialized = false;
+    bool m_gradientEnabled = true;
 };
-
